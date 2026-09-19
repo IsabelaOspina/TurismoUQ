5ee8790827dff2aa1f5ae2106413e6e44f63ad79
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2026-09-19 14:10:01 -0500

    feat: Se creo el repositorio y se realizo la conexion con la base de datos

---
+++ src/main/java/org/example/turismouq/TurismoUqApplication.java
@@ -0,0 +1,13 @@
+package org.example.turismouq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismoUqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurismoUqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/turismouq/TurismoUqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.turismouq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismoUqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
